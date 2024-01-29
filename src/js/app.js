c18c703fe67b749592c148f4f1302190e0645292
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-01-29 17:48:38 -0300

    última funcion del creada

--- src/js/app.js
+++ src/js/app.js
@@ -33,18 +33,3 @@ function iniciarApp() {
   mostrarResumen()
 }
 
-function mostrarResumen() {
-  // destructuring
-  const { nombre, fecha, servicios, hora } = cita
-  const resumen = document.querySelector(".contenido-resumen")
-
-  // validacion de objeto
-  if (Object.values(cita).includes("")) {
-    const sinCita = document.createElement("P")
-    sinCita.textContent = "Faltan servicios, fecha, hora o nombre"
-    sinCita.classList.add("invalidar-cita")
-    resumen.append(sinCita)
-  }
-
-  // TODO: continuar video 16
-}
